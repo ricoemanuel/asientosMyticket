ecb1236def5303e7b609f901a5984ee915f7bf81
unknown <erico@enovel.com.co>
2023-09-13 15:01:33 -0500

    cambio de credenciales

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,10 +1,11 @@
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyA6jOgwDaEqyUUCUZocqzjP0CRdoVhJ0zI",
-        authDomain: "myticketselector.firebaseapp.com",
-        projectId: "myticketselector",
-        storageBucket: "myticketselector.appspot.com",
-        messagingSenderId: "431174019860",
-        appId: "1:431174019860:web:12bdc5d227e30a3a7bb001",
-        measurementId: "G-RVLDTMY210"
-    },};
+        projectId: 'myticketeventos',
+        appId: '1:290015062564:web:d1770a05acca36952cc6b1',
+        storageBucket: 'myticketeventos.appspot.com',
+        apiKey: 'AIzaSyDrmlLtmYc-pDGovvGvlJABEv4aDWgK_x8',
+        authDomain: 'myticketeventos.firebaseapp.com',
+        messagingSenderId: '290015062564',
+        measurementId: 'G-932QCRPQWS',
+    },
+};
